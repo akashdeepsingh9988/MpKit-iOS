c30b8d525dfc8f6d0aba9f02bcc67e24c9306306
MacStudent <macstudent@128-imac15.local>
2018-11-13 08:33:27 -0500

    map added

--- MpKit/ViewController.swift
+++ MpKit/ViewController.swift
@@ -7,13 +7,28 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mpKit: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+        centerMapOnLocation(location: initialLocation)
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    let regionRadius: CLLocationDistance = 1000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius, regionRadius)
+        mpKit.setRegion(coordinateRegion, animated: true)
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
